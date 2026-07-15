815c47e92c547e0f995558aa73dbcdf75dfb15fc
Ash-exwo <aswathygr61@gmail.com>
2026-07-15 18:21:27 +0530

    sign-up api done

--- app.js
+++ app.js
@@ -3,13 +3,45 @@ const Mongoose = require("mongoose")
 const Bcrypt = require("bcrypt")
 const Cors = require("cors")
 const jwt = require("jsonwebtoken")
+const userModel = require("./models/users")
 
 let app = Express()
 
-app.get("/", (req,res)=>{
-    res.send("hello")
-})
+app.use(Express.json())
+app.use(Cors())
 
-app.listen(3030, ()=>{
+Mongoose.connect("mongodb://aswathy:ashexhere22@ac-d01d9ty-shard-00-00.36nhatr.mongodb.net:27017,ac-d01d9ty-shard-00-01.36nhatr.mongodb.net:27017,ac-d01d9ty-shard-00-02.36nhatr.mongodb.net:27017/blogdb?ssl=true&replicaSet=atlas-sg5pws-shard-0&authSource=admin&appName=Cluster0")
+
+app.post("/sign-up", async (req,res)=>{
+    // input
+    let input = req.body
+    //password encryption
+    let hashedPassword = Bcrypt.hashSync(req.body.password,10)
+    console.log(hashedPassword)
+    req.body.password = hashedPassword
+    userModel.find({email:req.body.email}).then(
+        (items)=>{
+            //to check whether the entered email is alredy in the db or not - search result comes inside the "items" which return an array
+
+            // console.log(items)
+            if (items.length>0) { //if "check" s length>0 ? then mail id already exists
+                res.json({"status":"emailId already exists"})
+            } else {
+                let result = new userModel(input) //passed the data to userModel if the email id didnt exists
+                result.save() //store data - async-await() - mongodb colud or any n/w application storing issues will be done in the bg -safety method -asynchronous fn
+                res.json({"status":"success"}) //1 api - 1 res
+            }
+        }
+    ).catch(
+        (error)=>{
+
+        }
+    )
+
+    })
+
+app.listen(3001, ()=>{
     console.log("Server Started")
 })
+
+
